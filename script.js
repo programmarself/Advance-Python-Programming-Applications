4a9a5ab1ea94f8f009d9a14bc409b2e4c0651ffa
IRFAN ULLAH KHAN <134633289+programmarself@users.noreply.github.com>
2025-06-22 20:47:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -18,9 +18,121 @@ document.addEventListener("DOMContentLoaded", () => {
       filename: "Iterators.ipynb",
       url: "notebooks/Iterators.ipynb"
     },
-     {
+    {
       filename: "17-Selecting-Elements.ipynb",
       url: "notebooks/17-Selecting-Elements.ipynb"
+    },
+    {
+      filename: "Functions.ipynb",
+      url: "notebooks/Functions.ipynb"
+    },
+    {
+      filename: "Lamda functions-Part1.ipynb",
+      url: "notebooks/Lamda functions-Part1.ipynb"
+    },
+    {
+      filename: "Lamda functions-Part2.ipynb",
+      url: "notebooks/Lamda functions-Part2.ipynb"
+    },
+    {
+      filename: "List Comprehension.ipynb",
+      url: "notebooks/List Comprehension.ipynb"
+    },
+    {
+      filename: "Numpy_Functions.ipynb",
+      url: "notebooks/Numpy_Functions.ipynb"
+    },
+    {
+      filename: "Numpy_lec1-part-a.ipynb",
+      url: "notebooks/Numpy_lec1-part-a.ipynb"
+    },
+    {
+      filename: "Numpy_lec2-3.ipynb",
+      url: "notebooks/Numpy_lec2-3.ipynb"
+    },
+    {
+      filename: "Python Dictionaries.ipynb",
+      url: "notebooks/Python Dictionaries.ipynb"
+    },
+    {
+      filename: "Python Exceptions.ipynb",
+      url: "notebooks/Python Exceptions.ipynb"
+    },
+    {
+      filename: "Python Lists.ipynb",
+      url: "notebooks/Python Lists.ipynb"
+    },
+    {
+      filename: "Python Tuples.ipynb",
+      url: "notebooks/Python Tuples.ipynb"
+    },
+    {
+      filename: "Spam_ClassificationP1-7-May-2025.ipynb",
+      url: "notebooks/Spam_ClassificationP1-7-May-2025.ipynb"
+    },
+    {
+      filename: "Spam_ClassificationP2-9-5-2025.ipynb",
+      url: "notebooks/Spam_ClassificationP2-9-5-2025.ipynb"
+    },
+    {
+      filename: "Deep_learning.ipynb",
+      url: "notebooks/Deep_learning.ipynb"
+    },
+    {
+      filename: "getcwd().ipynb",
+      url: "notebooks/getcwd().ipynb"
+    },
+    {
+      filename: "KNN.ipynb",
+      url: "notebooks/KNN.ipynb"
+    },
+    {
+      filename: "n_gram of spam classification.ipynb",
+      url: "notebooks/n_gram of spam classification.ipynb"
+    },
+    {
+      filename: "n_gram of urdu dataset.ipynb",
+      url: "notebooks/n_gram of urdu dataset.ipynb"
+    },
+    {
+      filename: "OOP_All_in_One.ipynb",
+      url: "notebooks/OOP_All_in_One.ipynb"
+    },
+    {
+      filename: "OOP1_Lecture1.ipynb",
+      url: "notebooks/OOP1_Lecture1.ipynb"
+    },
+    {
+      filename: "Python Files.ipynb",
+      url: "notebooks/Python Files.ipynb"
+    },
+    {
+      filename: "Spam_Classification-16-May.ipynb",
+      url: "notebooks/Spam_Classification-16-May.ipynb"
+    },
+    {
+      filename: "Spam_ClassificationP3-14-May.ipynb",
+      url: "notebooks/Spam_ClassificationP3-14-May.ipynb"
+    },
+    {
+      filename: "Spam_Classification-bigrams.ipynb",
+      url: "notebooks/Spam_Classification-bigrams.ipynb"
+    },
+    {
+      filename: "English.csv",
+      url: "notebooks/English.csv"
+    },
+    {
+      filename: "imdb_urdu_reviews_test.csv",
+      url: "notebooks/imdb_urdu_reviews_test.csv"
+    },
+    {
+      filename: "imdb_urdu_reviews_train.csv",
+      url: "notebooks/imdb_urdu_reviews_train.csv"
+    },
+    {
+      filename: "spam.csv",
+      url: "notebooks/spam.csv"
     }
   ];
 
