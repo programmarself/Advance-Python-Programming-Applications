252469cbe5bdddf3810c92fe618dc357195aa874
IRFAN ULLAH KHAN <134633289+programmarself@users.noreply.github.com>
2025-06-24 00:26:37 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,79 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const notebookList = document.getElementById("notebook-list");
 
-  // Reordered notebooks from basics to advanced
   const notebooks = [
-    // Python Fundamentals
-    {
-      filename: "Python Lists.ipynb",
-      url: "notebooks/Python Lists.ipynb"
-    },
-    {
-      filename: "Python Tuples.ipynb",
-      url: "notebooks/Python Tuples.ipynb"
-    },
-    {
-      filename: "Python Dictionaries.ipynb",
-      url: "notebooks/Python Dictionaries.ipynb"
-    },
-    {
-      filename: "Functions.ipynb",
-      url: "notebooks/Functions.ipynb"
-    },
-    {
-      filename: "Iterators.ipynb",
-      url: "notebooks/Iterators.ipynb"
-    },
-    {
-      filename: "List Comprehension.ipynb",
-      url: "notebooks/List Comprehension.ipynb"
-    },
-    {
-      filename: "Lamda functions-Part1.ipynb",
-      url: "notebooks/Lamda functions-Part1.ipynb"
-    },
-    {
-      filename: "Lamda functions-Part2.ipynb",
-      url: "notebooks/Lamda functions-Part2.ipynb"
-    },
-    {
-      filename: "Python Exceptions.ipynb",
-      url: "notebooks/Python Exceptions.ipynb"
-    },
-    {
-      filename: "Python Files.ipynb",
-      url: "notebooks/Python Files.ipynb"
-    },
-    {
-      filename: "getcwd().ipynb",
-      url: "notebooks/getcwd().ipynb"
-    },
-
-    // Object-Oriented Programming (OOP)
-    {
-      filename: "OOP1_Lecture1.ipynb",
-      url: "notebooks/OOP1_Lecture1.ipynb"
-    },
-    {
-      filename: "OOP_All_in_One.ipynb",
-      url: "notebooks/OOP_All_in_One.ipynb"
-    },
-
-    // Numerical Computing (NumPy)
-    {
-      filename: "Numpy_lec1-part-a.ipynb",
-      url: "notebooks/Numpy_lec1-part-a.ipynb"
-    },
-    {
-      filename: "Numpy_lec2-3.ipynb",
-      url: "notebooks/Numpy_lec2-3.ipynb"
-    },
-    {
-      filename: "Numpy_Functions.ipynb",
-      url: "notebooks/Numpy_Functions.ipynb"
-    },
-
-    // Data Analysis (Pandas)
     {
       filename: "12-13-Pandas Intro.ipynb",
       url: "notebooks/12-13-Pandas Intro.ipynb"
@@ -87,14 +15,56 @@ document.addEventListener("DOMContentLoaded", () => {
       url: "notebooks/16-Pandas%20Dataframes-Summary%20Statistics.ipynb"
     },
     {
+      filename: "Iterators.ipynb",
+      url: "notebooks/Iterators.ipynb"
+    },
+    {
       filename: "17-Selecting-Elements.ipynb",
       url: "notebooks/17-Selecting-Elements.ipynb"
     },
-
-    // Machine Learning & NLP
     {
-      filename: "KNN.ipynb",
-      url: "notebooks/KNN.ipynb"
+      filename: "Functions.ipynb",
+      url: "notebooks/Functions.ipynb"
+    },
+    {
+      filename: "Lamda functions-Part1.ipynb",
+      url: "notebooks/Lamda functions-Part1.ipynb"
+    },
+    {
+      filename: "Lamda functions-Part2.ipynb",
+      url: "notebooks/Lamda functions-Part2.ipynb"
+    },
+    {
+      filename: "List Comprehension.ipynb",
+      url: "notebooks/List Comprehension.ipynb"
+    },
+    {
+      filename: "Numpy_Functions.ipynb",
+      url: "notebooks/Numpy_Functions.ipynb"
+    },
+    {
+      filename: "Numpy_lec1-part-a.ipynb",
+      url: "notebooks/Numpy_lec1-part-a.ipynb"
+    },
+    {
+      filename: "Numpy_lec2-3.ipynb",
+      url: "notebooks/Numpy_lec2-3.ipynb"
+    },
+    {
+      filename: "Python Dictionaries.ipynb",
+      url: "notebooks/Python Dictionaries.ipynb"
+    },
+    {
+      filename: "Python Exceptions.ipynb",
+      url: "notebooks/Python Exceptions.ipynb"
+    },
+    {
+      filename: "Python Lists.ipynb",
+      url: "notebooks/Python Lists.ipynb"
+    },
+    {
+      filename: "Python Tuples.ipynb",
+      url: "notebooks/Python Tuples.ipynb"
     },
     {
       filename: "Spam_ClassificationP1-7-May-2025.ipynb",
@@ -105,16 +75,16 @@ document.addEventListener("DOMContentLoaded", () => {
       url: "notebooks/Spam_ClassificationP2-9-5-2025.ipynb"
     },
     {
-      filename: "Spam_ClassificationP3-14-May.ipynb",
-      url: "notebooks/Spam_ClassificationP3-14-May.ipynb"
+      filename: "Deep_learning.ipynb",
+      url: "notebooks/Deep_learning.ipynb"
     },
     {
-      filename: "Spam_Classification-16-May.ipynb",
-      url: "notebooks/Spam_Classification-16-May.ipynb"
+      filename: "getcwd().ipynb",
+      url: "notebooks/getcwd().ipynb"
     },
     {
-      filename: "Spam_Classification-bigrams.ipynb",
-      url: "notebooks/Spam_Classification-bigrams.ipynb"
+      filename: "KNN.ipynb",
+      url: "notebooks/KNN.ipynb"
     },
     {
       filename: "n_gram of spam classification.ipynb",
@@ -125,30 +95,44 @@ document.addEventListener("DOMContentLoaded", () => {
       url: "notebooks/n_gram of urdu dataset.ipynb"
     },
     {
-      filename: "Deep_learning.ipynb",
-      url: "notebooks/Deep_learning.ipynb"
+      filename: "OOP_All_in_One.ipynb",
+      url: "notebooks/OOP_All_in_One.ipynb"
     },
-
-    // Datasets
     {
-      filename: "spam.csv",
-      url: "notebooks/spam.csv"
+      filename: "OOP1_Lecture1.ipynb",
+      url: "notebooks/OOP1_Lecture1.ipynb"
+    },
+    {
+      filename: "Python Files.ipynb",
+      url: "notebooks/Python Files.ipynb"
+    },
+    {
+      filename: "Spam_Classification-16-May.ipynb",
+      url: "notebooks/Spam_Classification-16-May.ipynb"
+    },
+    {
+      filename: "Spam_ClassificationP3-14-May.ipynb",
+      url: "notebooks/Spam_ClassificationP3-14-May.ipynb"
+    },
+    {
+      filename: "Spam_Classification-bigrams.ipynb",
+      url: "notebooks/Spam_Classification-bigrams.ipynb"
     },
     {
       filename: "English.csv",
       url: "notebooks/English.csv"
     },
     {
-      filename: "imdb_urdu_reviews_train.csv",
-      url: "notebooks/imdb_urdu_reviews_train.csv"
-    },
-    {
       filename: "imdb_urdu_reviews_test.csv",
       url: "notebooks/imdb_urdu_reviews_test.csv"
     },
     {
-      filename: "Advanced Python Programming and Applications.pdf",
-      url: "notebooks/Advanced Python Programming and Applications.pdf"
+      filename: "imdb_urdu_reviews_train.csv",
+      url: "notebooks/imdb_urdu_reviews_train.csv"
+    },
+    {
+      filename: "spam.csv",
+      url: "notebooks/spam.csv"
     }
   ];
 
